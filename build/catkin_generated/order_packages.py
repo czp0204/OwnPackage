8374394264e0abd2a735231dd5ad97cf118eb93a
czp0204 <czp020418@163.com>
2025-03-11 12:25:57 +0800

    remove bulild space and devel space

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/czp/practice/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/czp/practice/devel;/home/czp/Livox_Driver/src/ws_livox/devel;/opt/ros/noetic'.split(';') if '/home/czp/practice/devel;/home/czp/Livox_Driver/src/ws_livox/devel;/opt/ros/noetic' != '' else []
